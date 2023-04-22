390e8a9b96ff4a4f79ecf5105239ff9a4dbb60e5
pjleonard37 <pjleonard37@gmail.com>
2023-04-22 12:18:38 -0400

    Add more types

--- GEOSwiftMapboxMaps/Sources/GEOSwiftMapboxMaps/GEOSwift+MapboxMaps.swift.swift
+++ GEOSwiftMapboxMaps/Sources/GEOSwiftMapboxMaps/GEOSwift+MapboxMaps.swift.swift
@@ -7,3 +7,52 @@ public extension CLLocationCoordinate2D {
         self.init(latitude: point.y, longitude: point.x)
     }
 }
+
+public extension GEOSwift.Point {
+    init(longitude: Double, latitude: Double) {
+        self.init(x: longitude, y: latitude)
+    }
+
+    init(_ coordinate: CLLocationCoordinate2D) {
+        self.init(x: coordinate.longitude, y: coordinate.latitude)
+    }
+}
+
+public extension GEOSwift.Polygon {
+    // swiftlint:disable:next force_try
+    static let world = try! GEOSwift.Polygon(
+        exterior: Polygon.LinearRing(
+            points: [
+                Point(x: -180, y: 90),
+                Point(x: -180, y: -90),
+                Point(x: 180, y: -90),
+                Point(x: 180, y: 90),
+                Point(x: -180, y: 90)]))
+}
+
+public extension PointAnnotation {
+    init(point: GEOSwift.Point) {
+        let coordinate = CLLocationCoordinate2D(point)
+        self.init(coordinate: coordinate)
+    }
+}
+
+public extension Turf.LineString {
+    init(lineString: GEOSwift.LineString) {
+        let points = lineString.points
+        let coordinates = points.map { point in
+            LocationCoordinate2D(point)
+        }
+        self.init(coordinates)
+    }
+}
+
+public extension GEOSwift.LineString {
+    init(lineString: Turf.LineString) throws {
+        let coordinates = lineString.coordinates
+        let points = coordinates.map { coordinate in
+            GEOSwift.Point(longitude: coordinate.longitude, latitude: coordinate.latitude)
+        }
+        try self.init(points: points)
+    }
+}
--- GEOSwiftMapboxMaps/Tests/GEOSwiftMapboxMapsTests/GEOSwiftMapboxMapsTests.swift
+++ GEOSwiftMapboxMaps/Tests/GEOSwiftMapboxMapsTests/GEOSwiftMapboxMapsTests.swift
@@ -5,11 +5,81 @@ import GEOSwift
 @testable import GEOSwiftMapboxMaps
 
 final class GEOSwiftMapboxMapsTests: XCTestCase {
+    
+    // CLLocationCoordinate2D
+    
     func testCreateCLLocationCoordinate2DFromPoint() {
         let point = Point(x: 45, y: 9)
 
         let coordinate = CLLocationCoordinate2D(point)
 
         XCTAssertEqual(coordinate, CLLocationCoordinate2D(latitude: 9, longitude: 45))
+    }
+    
+    // Point
+    func testCreatePointWithLatAndLong() {
+        let point = Point(longitude: 1, latitude: 2)
+
+        XCTAssertEqual(point.x, 1)
+        XCTAssertEqual(point.y, 2)
+    }
+
+    func testCreatePointWithCLLocationCoordinate2D() {
+        let coord = CLLocationCoordinate2D(latitude: 2, longitude: 1)
+
+        XCTAssertEqual(Point(coord), Point(x: 1, y: 2))
+    }
+    
+    // Polygon
+    func testWorldPolygon() {
+        // just make sure it doesn't crash
+        _ = GEOSwift.Polygon.world
+    }
+    
+    // PointAnnotation
+    func testCreatePointAnnotationFromPoint() {
+        let point = Point(x: 45, y: 9)
+
+        let annotation = PointAnnotation(point: point)
+        
+
+        guard case let .point(annotationGeometry) = annotation.geometry else {
+            XCTFail("Incorrect geometry type.")
+            return
+        }
+
+        XCTAssertEqual(annotationGeometry.coordinates, CLLocationCoordinate2D(latitude: 9, longitude: 45))
+    }
+    
+    // LineString
+    func testCreateTurfLineStringFromGEOSwiftLineString() {
+        let geoSwiftLineString = try! GEOSwift.LineString(wkt: "LINESTRING(3 4,10 50,20 25)")
+
+        let turfLineString = Turf.LineString(lineString: geoSwiftLineString)
+
+        XCTAssertEqual(geoSwiftLineString.points.count, turfLineString.coordinates.count)
+        XCTAssertEqual(geoSwiftLineString.firstPoint.x, turfLineString.coordinates.first?.longitude)
+        XCTAssertEqual(geoSwiftLineString.lastPoint.x, turfLineString.coordinates.last?.longitude)
+    }
+    
+    func testCreateGEOSwiftLineStringFromTurfLineString() {
+        let coordinates: [LocationCoordinate2D] = [
+            LocationCoordinate2D(latitude: 0, longitude: 4),
+            LocationCoordinate2D(latitude: 1, longitude: 3),
+            LocationCoordinate2D(latitude: 2, longitude: 2),
+            LocationCoordinate2D(latitude: 3, longitude: 1),
+            LocationCoordinate2D(latitude: 4, longitude: 0)
+        ]
+        
+        let turfLineString = Turf.LineString(coordinates)
+        
+        guard let geoSwiftLineString = try? GEOSwift.LineString(lineString: turfLineString) else {
+            XCTFail("Conversion failed.")
+            return
+        }
+        
+        XCTAssertEqual(geoSwiftLineString.points.count, turfLineString.coordinates.count)
+        XCTAssertEqual(geoSwiftLineString.firstPoint.x, turfLineString.coordinates.first?.longitude)
+        XCTAssertEqual(geoSwiftLineString.lastPoint.x, turfLineString.coordinates.last?.longitude)
     }
 }
